f5fcc4073ffbb1b4f3394e89eae6446d79b75ec6
Peter Obijiofor <108196402+DoingsLLC@users.noreply.github.com>
2023-12-30 14:15:30 -0500

    Update backend-s3.tf

--- terraform-aws-vprofile-project/backend-s3.tf
+++ terraform-aws-vprofile-project/backend-s3.tf
@@ -4,5 +4,20 @@ terraform {
     key    = "terraform/backend"
     region = "us-east-1"
   }
-depends_on = [aws_s3_bucket.doings-s3]
+}
+
+resource "aws_s3_bucket" "doings-s3" {
+  # Your AWS S3 bucket configuration goes here
+  bucket = "terra-doings-state"
+
+  tags = {
+    Name        = "Doings bucket"
+    Environment = "Dev-Prod"
+  }
+}
+
+# Declare a dependency of the aws_s3_bucket resource on the terraform block
+# This ensures that the S3 bucket is created before configuring the backend
+resource "null_resource" "dependency" {
+  depends_on = [aws_s3_bucket.doings-s3]
 }

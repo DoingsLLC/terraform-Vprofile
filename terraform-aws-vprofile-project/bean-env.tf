d9cc4da8ee309a34196947bd0fe6fdfe28695018
Peter Obijiofor <108196402+DoingsLLC@users.noreply.github.com>
2023-12-31 17:31:23 -0500

    Update bean-env.tf

--- terraform-aws-vprofile-project/bean-env.tf
+++ terraform-aws-vprofile-project/bean-env.tf
@@ -1,7 +1,7 @@
 resource "aws_elastic_beanstalk_environment" "doingsvprofile-bean-prod" {
   name                = "doingsvprofile-bean-prod"
   application         = aws_elastic_beanstalk_application.doingsvprofile-prod.name
-  solution_stack_name = "64bit Amazon Linux 2023 v5.1.2 running Tomcat 10 Corretto 17"
+  solution_stack_name = "64bit Amazon Linux 2023 v5.1.2 running Tomcat 9 Corretto 11"
   # cname_prefix        = "doingsvprofile-bean-prod-domain"
   setting {
     name      = "VPCId"
